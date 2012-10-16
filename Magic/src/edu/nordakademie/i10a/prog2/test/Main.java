fb8a44df1dc083571dcda528743a36986ccf79f2
Karol Herbst <git@karolherbst.de>
2012-10-16 20:28:36 +0200

    Add Main class

---
+++ Magic/src/edu/nordakademie/i10a/prog2/test/Main.java
@@ -0,0 +1,12 @@
+package edu.nordakademie.i10a.prog2.test;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
