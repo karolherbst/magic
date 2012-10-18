4ac596b5fe6bbb30d7360e94f69bd508eed98d13
Karol Herbst <git@karolherbst.de>
2012-10-18 18:15:01 +0200

    Set UserInterface as Superinterface to GameClient

--- src/main/java/herbstJennrichLehmannRitter/client/GameClient.java
+++ src/main/java/herbstJennrichLehmannRitter/client/GameClient.java
@@ -1,8 +1,10 @@
 package herbstJennrichLehmannRitter.client;
 
+import herbstJennrichLehmannRitter.ui.UserInterface;
+
 /**
  * Der GameClient ist die Antwortschnittstelle des Server auf dem Client
  */
-public interface GameClient {
+public interface GameClient extends UserInterface {
 
 }
