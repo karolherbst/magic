dc29f536c9277fe695084fee429edc87820bc4a1
Sebastian Jennrich <sebastian.jennrich@gmail.de>
2012-10-18 21:15:12 +0200

    add Javadoc to DataImpl

--- src/main/java/herbstJennrichLehmannRitter/engine/model/impl/DataImpl.java
+++ src/main/java/herbstJennrichLehmannRitter/engine/model/impl/DataImpl.java
@@ -3,13 +3,19 @@ package herbstJennrichLehmannRitter.engine.model.impl;
 import herbstJennrichLehmannRitter.engine.model.Data;
 import herbstJennrichLehmannRitter.engine.model.Player;
 
-// TODO Muss hier auch Javadoc hin?
+/**
+ * Implementation von Data.<br />
+ * Die Spielerobjekte werden in dieser Klasse gespeichert und 
+ * von den GameClients ({@link herbstJennrichLehmannRitter.client.GameClient}) genutzt, um die 
+ * beiden Spielerobjekte abzurufen.
+ *
+ */
 public class DataImpl implements Data {
 
 	private Player ownPlayer;
 	private Player enemyPlaner;
 	
-	// TODO wie werden ownPlayer und enemyPlayer gesetzt? 
+	// TODO wie und wo werden ownPlayer und enemyPlayer eigentlich gesetzt? 
 	
 	public Player getOwnPlayer() {
 		return this.ownPlayer;
