a7292780e3f00781c0f9b68539ad5fcacd698608
Karol Herbst <git@karolherbst.de>
2012-10-23 12:55:54 +0200

    complex card changes

--- src/main/java/herbstJennrichLehmannRitter/engine/model/action/complexImpl/BarrackeComplexAction.java
+++ src/main/java/herbstJennrichLehmannRitter/engine/model/action/complexImpl/BarrackeComplexAction.java
@@ -1,13 +1,16 @@
 package herbstJennrichLehmannRitter.engine.model.action.complexImpl;
 
+import herbstJennrichLehmannRitter.engine.annotation.ComplexCard;
 import herbstJennrichLehmannRitter.engine.model.Player;
 import herbstJennrichLehmannRitter.engine.model.action.ComplexCardAction;
 
+@ComplexCard("Barracke")
 public class BarrackeComplexAction implements ComplexCardAction {
 
 	@Override
 	public void applyActionOnPlayer(Player sourcePlayer, Player targetPlayer) {
-		
+		if (sourcePlayer.getDungeon().getLevel() < targetPlayer.getDungeon().getLevel()) {
+			sourcePlayer.getDungeon().setLevel(sourcePlayer.getDungeon().getLevel() + 1);
+		}
 	}
-
 }
