b463a3adcf60c5aaf9235ba4241e0265c3bab8db
Sebastian Jennrich <sebastian.jennrich@gmail.de>
2012-10-23 12:45:07 +0200

    add some Cards

--- src/main/java/herbstJennrichLehmannRitter/engine/model/action/ComplexCardAction.java
+++ src/main/java/herbstJennrichLehmannRitter/engine/model/action/ComplexCardAction.java
@@ -9,7 +9,7 @@ import herbstJennrichLehmannRitter.engine.model.Player;
  * Schaden und Kartenzuteilung hinausgehen 
  * 
  * Steinbruch-Karten: Barracke, Demolieren, Grundstein, Hauptader, Spionage, Verschiebung
- * Zauberlabor-Karten: Baukoloss, Blitz und Donner, Glasperlen,  
+ * Zauberlabor-Karten: Baukoloss, Blitz und Donner, Glasperlen, Parität, Prisma, Pure Magie, Quarz, Rauchquarz, Weihnachtsmann  
  *
  *Verslies Karten: Blutmond, Dieb, Einhorn, Elfische Bogenschützen, Elfischer Späher,
  *Fee, 
