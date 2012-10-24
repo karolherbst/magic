d39dd61e50c2e923fa1dc954197714c8b31af32d
Sebastian Jennrich <sebastian.jennrich@gmail.de>
2012-10-24 14:53:24 +0200

    DeckTest

---
+++ src/test/java/herbstJennrichLehmannRitter/tests/model/DeckTests.java
@@ -0,0 +1,57 @@
+package herbstJennrichLehmannRitter.tests.model;
+
+import static org.junit.Assert.fail;
+import herbstJennrichLehmannRitter.engine.factory.impl.PlayerFactoryImpl;
+import herbstJennrichLehmannRitter.engine.model.Player;
+import herbstJennrichLehmannRitter.engine.model.impl.PlayerImpl;
+
+import org.junit.Test;
+
+public class DeckTests {
+
+	@Test
+	public void testDeckStageShuffle() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testHandDeckDiscardCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiscardAllCards() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiscardAllCardsByType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPickCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPickCards() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPickCardFromDeckStackOrCemeteryDeckWithCostAbout() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPickNumberOfCardsWithType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExchangeCardsWithHandDeck() {
+		fail("Not yet implemented");
+	}
+
+}
