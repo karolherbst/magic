4ddf2f21136902e629cfd1d5a4e97a336132174b
Daniel <Daniel@Daniels-Laptop>
2012-11-04 15:03:18 +0100

    implemented some labels, preliminary backup

--- src/main/java/herbstJennrichLehmannRitter/ui/GUI/PlayGameGUI.java
+++ src/main/java/herbstJennrichLehmannRitter/ui/GUI/PlayGameGUI.java
@@ -3,10 +3,14 @@ package herbstJennrichLehmannRitter.ui.GUI;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 public class PlayGameGUI {
 
@@ -18,6 +22,32 @@ public class PlayGameGUI {
 	private Shell shell;
 	private final Display display;
 	private Button btnExit;
+	private Button btnCardOne;
+	private Button btnCardTwo;
+	private Button btnCardThree;
+	private Button btnCardFour;
+	private Button btnCardFive;
+	private Button btnCardSix;
+	private Button btnPlayingCardOne;
+	private Button btnPlayingCardTwo;
+	private Label Enemy;
+	private Label Player;
+	private Label EnemyDungeonLevel;
+	private Label EnemyDungeonRessources;
+	private Label EnemyLabLevel;
+	private Label EnemyLabRessources;
+	private Label EnemyMineLevel;
+	private Label EnemyMineRessources;
+	private Label EnemyTowerLevel;
+	private Label EnemyWallLevel;
+	private Label PlayerDungeonLevel;
+	private Label PlayerDungeonRessources;
+	private Label PlayerLabLevel;
+	private Label PlayerLabRessources;
+	private Label PlayerMineLevel;
+	private Label PlayerMineRessources;
+	private Label PlayerTowerLevel;
+	private Label PlayerWallLevel;
 	
 	
 	
@@ -27,15 +57,25 @@ public class PlayGameGUI {
 		initBtnExit();
 	}
 	
-	public void open() {
-		this.shell.open();
+
+	private void initShell() {
+		this.shell = new Shell(SWT.TITLE | SWT.CLOSE);
+		this.shell.setText("Spiel");
+		this.shell.setLayout(new FormLayout());
+		this.shell.layout();
+		this.shell.pack();
+		this.shell.setSize(1024, 600);
 	}
 	
 	private void initBtnExit() {
-		this.btnExit = new Button(this.shell, SWT.NONE);
-		this.btnExit.setText("Schließen");
-		this.btnExit.setLayoutData(new GridData(GridData.FILL, GridData.END,
-				true, true));
+		this.btnExit = new Button(this.shell, SWT.PUSH | SWT.NONE);
+		FormData btnExitData = new FormData();
+		btnExitData.left =  new FormAttachment(0, 2, 2);
+		btnExitData.top =  new FormAttachment(0, 1000, 540);
+		btnExitData.width = 1010;
+		btnExitData.height = 30;
+		this.btnExit.setLayoutData(btnExitData);
+		this.btnExit.setText("Spiel beenden");
 		this.btnExit.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -44,11 +84,10 @@ public class PlayGameGUI {
 		});
 	}
 
-	private void initShell() {
-		this.shell = new Shell(SWT.TITLE | SWT.CLOSE);
-		this.shell.setText("Spiel");
-		this.shell.setLayout(new GridLayout(6, false));
-		this.shell.setSize(1024, 600);
+	public void open() {
+		this.shell.open();
 	}
+	
+	
 
 }
