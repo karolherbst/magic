9d0af0feb684df86524bd4560d2b52f550302b06
Sönke Lehmann <soenke@lehmann-sh.de>
2012-11-08 17:09:52 +0100

    Update setData Methode -> correct? \(x)-(x)/

--- src/main/java/herbstJennrichLehmannRitter/ui/impl/ClientUserInterface.java
+++ src/main/java/herbstJennrichLehmannRitter/ui/impl/ClientUserInterface.java
@@ -4,18 +4,38 @@ import herbstJennrichLehmannRitter.engine.model.Card;
 import herbstJennrichLehmannRitter.engine.model.Data;
 import herbstJennrichLehmannRitter.ui.UserInterface;
 import herbstJennrichLehmannRitter.ui.GUI.MainMenuGUI;
+import herbstJennrichLehmannRitter.ui.GUI.PlayGameGUI;
 
 import java.util.Collection;
 
 public class ClientUserInterface implements UserInterface {
 
-	public ClientUserInterface(MainMenuGUI mainMenuGUI) {
-		
+	private PlayGameGUI playGameGUI;
+
+	public ClientUserInterface(PlayGameGUI playGameGUI) {
+		//TODO: Was hier?
+	setData(null);
 	}
 	
 	@Override
 	public void setData(Data data) {
-		// TODO Auto-generated method stub
+		// TODO: ist das von der Logik her so richtig?
+		playGameGUI.setPlayerDungeonLevel(data.getOwnPlayer().getDungeon().getLevel());
+		playGameGUI.setPlayerDungeonStock(data.getOwnPlayer().getDungeon().getStock());
+		playGameGUI.setPlayerMagicLabLevel(data.getOwnPlayer().getMagicLab().getLevel());
+		playGameGUI.setPlayerMagicLabStock(data.getOwnPlayer().getMagicLab().getStock());
+		playGameGUI.setPlayerMineLevel(data.getOwnPlayer().getMine().getLevel());
+		playGameGUI.setPlayerMineStock(data.getOwnPlayer().getMine().getStock());
+		playGameGUI.setPlayerTower(data.getOwnPlayer().getTower().getActualPoints());
+		playGameGUI.setPlayerWall(data.getOwnPlayer().getWall().getActualPoints());
+		playGameGUI.setEnemyDungeonLevel(data.getEnemyPlayer().getDungeon().getLevel());
+		playGameGUI.setEnemyDungeonStock(data.getEnemyPlayer().getDungeon().getStock());
+		playGameGUI.setEnemyMagicLabLevel(data.getEnemyPlayer().getMagicLab().getLevel());
+		playGameGUI.setEnemyMagicLabStock(data.getEnemyPlayer().getMagicLab().getStock());
+		playGameGUI.setEnemyMineLevel(data.getEnemyPlayer().getMine().getLevel());
+		playGameGUI.setEnemyMineStock(data.getEnemyPlayer().getMine().getStock());
+		playGameGUI.setEnemyTower(data.getEnemyPlayer().getTower().getActualPoints());
+		playGameGUI.setEnemyWall(data.getEnemyPlayer().getWall().getActualPoints());
 	}
 
 	@Override
