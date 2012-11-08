a31963fbdf7070ac88ceeb6da5ed400b52fd7a74
Sebastian Jennrich <sebastian.jennrich@gmail.de>
2012-11-08 13:26:18 +0100

    some gui changes

--- src/main/java/herbstJennrichLehmannRitter/ui/GUI/GameMenuGUI.java
+++ src/main/java/herbstJennrichLehmannRitter/ui/GUI/GameMenuGUI.java
@@ -7,7 +7,9 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 public class GameMenuGUI {
 
 	/**
@@ -17,59 +19,79 @@ public class GameMenuGUI {
 	
 	private Shell shell;
 	private final Display display;
+	private Label nameTextLabel;
+	private Text nameTextField;
+	private Label selectionTextLabel;
 	private Button startHostButton;
 	private Button startClientButton;
-	private Button exitButton;
+	private Button startLocalButton;
+	private Button backButton;
 	
 	//Subviews
 	private HostMenuGUI hostMenuGUI;
 	private ClientMenuGUI clientMenuGUI;
+	private PlayGameGUI playGameGUI;
 	
 	
 	public GameMenuGUI(Display parent) {
 		this.display = parent;
 		initShell();
+		initNameTextLabel();
+		initNameTextField();
+		initSelectionTextLabel();
 		initStartHostButton();
 		initStartClientButton();
-		initExitButton();
+		initStartLocalButton();
+		initBackButton();
+		this.shell.pack();
+		MainMenuGUI.setShellLocationCenteredToScreen(this.display, this.shell);
 		
 		this.hostMenuGUI = new HostMenuGUI(this.display);
 		this.clientMenuGUI = new ClientMenuGUI(this.display);
+		this.playGameGUI = new PlayGameGUI(this.display);
 	}
 	
 	public void open() {
 		this.shell.open();
 	}
 	
-	private void initExitButton() {
-		this.exitButton = new Button(this.shell, SWT.NONE);
-		this.exitButton.setText("Zurück");
-		this.exitButton.setLayoutData(new GridData(GridData.FILL, GridData.CENTER,
-				true, false));
-		this.exitButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				GameMenuGUI.this.shell.setVisible(false);
-			}
-		});
+	private void initShell() {
+		this.shell = new Shell(SWT.TITLE | SWT.CLOSE);
+		this.shell.setText("Spielauswahl");
+		this.shell.setLayout(new GridLayout(1, false));
 	}
-
-
-	private void initStartClientButton() {
-		this.startClientButton= new Button(this.shell, SWT.NONE);
-		this.startClientButton.setText("Starte als Client");
-		this.startClientButton.setToolTipText("An einem Spiel teilnehmen");
-		this.startClientButton.setLayoutData(new GridData(GridData.FILL, GridData.CENTER,
-				true, false));
-		this.startClientButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				GameMenuGUI.this.clientMenuGUI.open();
-			}
-		});
+	
+	private void initNameTextLabel() {
+	    GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.horizontalSpan = 3;
+		
+		this.nameTextLabel = new Label(this.shell, SWT.FILL);
+		this.nameTextLabel.setText("Bitte geben Sie ihren Namen an:");
+		this.nameTextLabel.setBackground(this.shell.getBackground());
+		this.nameTextLabel.setLayoutData(gridData);
 	}
+	
+	private void initNameTextField() {
+	    GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.horizontalSpan = 3;
+		
+		this.nameTextField = new Text(this.shell, SWT.FILL);
+		this.nameTextField.setLayoutData(gridData);
+	}	
 
-
+	private void initSelectionTextLabel() {
+	    GridData gridData = new GridData(GridData.FILL_HORIZONTAL);
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.horizontalSpan = 3;
+		
+		this.selectionTextLabel = new Label(this.shell, SWT.FILL);
+		this.selectionTextLabel.setText("Wählen Sie ihre Spieloption aus:");
+		this.selectionTextLabel.setBackground(this.shell.getBackground());
+		this.selectionTextLabel.setLayoutData(gridData);		
+	}
+	
 	private void initStartHostButton() {
 		this.startHostButton = new Button(this.shell, SWT.NONE);
 		this.startHostButton.setText("Starte als Host");
@@ -83,14 +105,45 @@ public class GameMenuGUI {
 			}
 		});
 	}
-
-
-	private void initShell() {
-		this.shell = new Shell(SWT.TITLE | SWT.CLOSE);
-		this.shell.setText("Spielauswahl");
-		this.shell.setLayout(new GridLayout(1, false));
-		this.shell.setSize(220, 145);
-		MainMenuGUI.setShellLocationCenteredToScreen(this.display, this.shell);
+	
+	private void initStartClientButton() {
+		this.startClientButton= new Button(this.shell, SWT.NONE);
+		this.startClientButton.setText("Starte als Client");
+		this.startClientButton.setToolTipText("An einem Spiel teilnehmen");
+		this.startClientButton.setLayoutData(new GridData(GridData.FILL, GridData.CENTER,
+				true, false));
+		this.startClientButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				GameMenuGUI.this.clientMenuGUI.open();
+			}
+		});
 	}
-
+	
+	private void initStartLocalButton() {
+		this.startLocalButton= new Button(this.shell, SWT.NONE);
+		this.startLocalButton.setText("Lokales Spiel starten");
+		this.startLocalButton.setToolTipText("Ein Spiel gegen die KI starten");
+		this.startLocalButton.setLayoutData(new GridData(GridData.FILL, GridData.CENTER,
+				true, false));
+		this.startLocalButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				GameMenuGUI.this.playGameGUI.open();
+			}
+		});
+	}
+	
+	private void initBackButton() {
+		this.backButton = new Button(this.shell, SWT.NONE);
+		this.backButton.setText("Zurück");
+		this.backButton.setLayoutData(new GridData(GridData.FILL, GridData.CENTER,
+				true, false));
+		this.backButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				GameMenuGUI.this.shell.setVisible(false);
+			}
+		});
+	}
 }
