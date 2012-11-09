db32ea4522b4d6334ac994299aaf490bbc82b3d3
Karol Herbst <git@karolherbst.de>
2012-11-09 08:37:14 +0100

    service: add resource to player at turn begin

--- src/main/java/herbstJennrichLehmannRitter/engine/service/impl/GameServiceImpl.java
+++ src/main/java/herbstJennrichLehmannRitter/engine/service/impl/GameServiceImpl.java
@@ -152,6 +152,7 @@ public class GameServiceImpl implements GameService {
 		UIHolder uiHolder = this.threadToUi.get(thread);
 		System.out.println("service: player " + uiHolder.player.getName() + " played card " + card.getName());
 		
+		this.gameEngineController.addResourcesToPlayer(uiHolder.player);
 		this.gameEngineController.playCard(card, uiHolder.player, uiHolder.enemy.player);
 		uiHolder.enemy.userInterface.enemeyPlayedCard(card);
 		
@@ -177,6 +178,7 @@ public class GameServiceImpl implements GameService {
 		UIHolder uiHolder = this.threadToUi.get(thread);
 		System.out.println("service: player " + uiHolder.player.getName() + " discard card " + card.getName());
 		
+		this.gameEngineController.addResourcesToPlayer(uiHolder.player);
 		this.gameEngineController.discardCard(card, uiHolder.player);
 		uiHolder.enemy.userInterface.nextTurn();
 		
