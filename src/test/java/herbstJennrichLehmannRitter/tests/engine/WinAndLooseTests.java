bdea34512c3ebeeb7263806c593942b2deced9d1
Sebastian Jennrich <sebastian.jennrich@gmail.de>
2012-11-16 18:57:04 +0100

    remove doubled tests and add some tests

--- src/test/java/herbstJennrichLehmannRitter/tests/engine/WinAndLooseTests.java
+++ src/test/java/herbstJennrichLehmannRitter/tests/engine/WinAndLooseTests.java
@@ -1,5 +1,6 @@
 package herbstJennrichLehmannRitter.tests.engine;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import herbstJennrichLehmannRitter.engine.Globals;
 import herbstJennrichLehmannRitter.engine.controller.WinAndLoseChecker;
@@ -14,189 +15,104 @@ import org.junit.Test;
 
 public class WinAndLooseTests {
 	private PlayerFactory playerFactory = new PlayerFactoryImpl();
-	private Player player1;
-	private Player player2;
+	private Player playerOne;
+	private Player playerTwo;
 
 	@Before
 	public void setUp() {
-
-		this.player1 = this.playerFactory.createPlayer("Player1", Globals.getGameCardFactory().createDefaultDeck(),
+		this.playerOne = this.playerFactory.createPlayer("Player1", Globals.getGameCardFactory().createDefaultDeck(),
+				10, 10, 10, 10);
+		this.playerTwo = this.playerFactory.createPlayer("Player2", Globals.getGameCardFactory().createDefaultDeck(),
 				0, 0, 0, 0);
-		this.player2 = this.playerFactory.createPlayer("Player2", Globals.getGameCardFactory().createDefaultDeck(),
-				0, 0, 0, 0);
-
 	}
 
 	@Test
-	public void testHasPlayer1LostWithPointsUnder0() {
-		this.player1.getTower().applyDamage(30);
-		this.player2.getTower().addPoints(10);
+	public void testHasPlayerLostWithPointsUnder0() {
+		this.playerOne.getTower().applyDamage(30);
 		WinAndLoseChecker winAndLoseChecker = new WinAndLoseTowerBuildingChecker();
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player1));
+		assertTrue(winAndLoseChecker.hasPlayerLost(this.playerOne));
 	}
 	
 	@Test
-	public void testHasPlayer2LostWithPointsUnder0() {
-		this.player2.getTower().applyDamage(30);
+	public void testHasPlayerLostWithPointsEquals0() {
+		this.playerOne.getTower().applyDamage(10);
 		WinAndLoseChecker winAndLoseChecker = new WinAndLoseTowerBuildingChecker();
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player2));
+		assertTrue(winAndLoseChecker.hasPlayerLost(this.playerOne));
 	}
-
+	
 	@Test
-	public void testHasPlayer1LostWithPointsEquals0() {
-		this.player1.getTower().applyDamage(25);
+	public void testHasPlayerLostWithPointsAbove0() {
+		this.playerOne.getTower().applyDamage(5);
 		WinAndLoseChecker winAndLoseChecker = new WinAndLoseTowerBuildingChecker();
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player1));
+		assertFalse(winAndLoseChecker.hasPlayerLost(this.playerOne));
 	}
 	
 	@Test
-	public void testHasPlayer2LostWithPointsEquals0() {
-		this.player2.getTower().applyDamage(25);
+	public void testHasPlayerWonWithTowerPointsEqual100() {
+		this.playerOne.getTower().addPoints(90);
 		WinAndLoseChecker winAndLoseChecker = new WinAndLoseTowerBuildingChecker();
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player2));
+		assertTrue(winAndLoseChecker.hasPlayerWon(this.playerOne));
 	}
 	
 	@Test
-	public void testHasPlayer1LostWithPointsAbove0() {
-		this.player1.getTower().applyDamage(20);
+	public void testHasPlayerWonWithTowerPointsAbove100() {
+		this.playerOne.getTower().addPoints(95);
 		WinAndLoseChecker winAndLoseChecker = new WinAndLoseTowerBuildingChecker();
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player1));
+		assertTrue(winAndLoseChecker.hasPlayerWon(this.playerOne));
 	}
 	
 	@Test
-	public void testHasPlayer2LostWithPointsAbove0() {
-		this.player2.getTower().applyDamage(20);
+	public void testHasPlayerWonWithTowerPointsUnder100() {
+		this.playerOne.getTower().addPoints(5);
 		WinAndLoseChecker winAndLoseChecker = new WinAndLoseTowerBuildingChecker();
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player2));
+		assertFalse(winAndLoseChecker.hasPlayerWon(this.playerOne));
 	}
 	
 	@Test
-	public void testHasPlayer1WonWithTowerPointsEqual100() {
-		this.player1.getTower().addPoints(100);
-		WinAndLoseChecker winAndLoseChecker = new WinAndLoseTowerBuildingChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player1));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player2));
-	}
-	
-	@Test
-	public void testHasPlayer2WonWithTowerPointsEqual100() {
-		this.player2.getTower().addPoints(100);
-		WinAndLoseChecker winAndLoseChecker = new WinAndLoseTowerBuildingChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player2));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player1));
-	}
-	
-	@Test
-	public void testHasPlayer1WonWithTowerPointsAbove100() {
-		this.player1.getTower().addPoints(105);
-		WinAndLoseChecker winAndLoseChecker = new WinAndLoseTowerBuildingChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player1));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player2));
-	}
-	
-	@Test
-	public void testHasPlayer2WonWithTowerPointsAbove100() {
-		this.player2.getTower().addPoints(105);
-		WinAndLoseChecker winAndLoseChecker = new WinAndLoseTowerBuildingChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player2));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player1));
-	}
-	
-	@Test
-	public void testHasPlayer1WonWithCrystalEquals400() {
-		this.player1.getMagicLab().addStock(400);
+	public void testHasPlayerWonWithCrystalEquals400() {
+		this.playerOne.getMagicLab().addStock(390);
 		WinAndLoseChecker winAndLoseChecker = new WinAndLoseResourceRageChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player1));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player2));
+		assertTrue(winAndLoseChecker.hasPlayerWon(this.playerOne));
+		assertTrue(winAndLoseChecker.hasPlayerLost(this.playerTwo));
 	}
 	
 	@Test
-	public void testHasPlayer2WonWithCrystalEquals400() {
-		this.player2.getMagicLab().addStock(400);
+	public void testHasPlayerWonWithBrickEquals400() {
+		this.playerOne.getMine().addStock(390);
 		WinAndLoseChecker winAndLoseChecker = new WinAndLoseResourceRageChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player2));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player1));
+		assertTrue(winAndLoseChecker.hasPlayerWon(this.playerOne));
+		assertTrue(winAndLoseChecker.hasPlayerLost(this.playerTwo));
 	}
 	
 	@Test
-	public void testHasPlayer1WonWithBrickEquals400() {
-		this.player1.getMine().addStock(400);
+	public void testHasPlayerWonWithMonstersEquals400() {
+		this.playerOne.getDungeon().addStock(390);
 		WinAndLoseChecker winAndLoseChecker = new WinAndLoseResourceRageChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player1));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player2));
+		assertTrue(winAndLoseChecker.hasPlayerWon(this.playerOne));
+		assertTrue(winAndLoseChecker.hasPlayerLost(this.playerTwo));
 	}
 	
 	@Test
-	public void testHasPlayer2WonWithBrickEquals400() {
-		this.player2.getMine().addStock(400);
+	public void testHasPlayerWonWithCrystalAbove400() {
+		this.playerOne.getMagicLab().addStock(444);
 		WinAndLoseChecker winAndLoseChecker = new WinAndLoseResourceRageChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player2));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player1));
+		assertTrue(winAndLoseChecker.hasPlayerWon(this.playerOne));
+		assertTrue(winAndLoseChecker.hasPlayerLost(this.playerTwo));
 	}
 	
 	@Test
-	public void testHasPlayer1WonWithMonstersEquals400() {
-		this.player1.getDungeon().addStock(400);
+	public void testHasPlayerWonWithBrickAbove400() {
+		this.playerOne.getMine().addStock(444);
 		WinAndLoseChecker winAndLoseChecker = new WinAndLoseResourceRageChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player1));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player2));
+		assertTrue(winAndLoseChecker.hasPlayerWon(this.playerOne));
+		assertTrue(winAndLoseChecker.hasPlayerLost(this.playerTwo));
 	}
 	
 	@Test
-	public void testHasPlayer2WonWithMonstersEquals400() {
-		this.player2.getDungeon().addStock(400);
+	public void testHasPlayerWonWithMonstersAbove400() {
+		this.playerOne.getDungeon().addStock(444);
 		WinAndLoseChecker winAndLoseChecker = new WinAndLoseResourceRageChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player2));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player1));
+		assertTrue(winAndLoseChecker.hasPlayerWon(this.playerOne));
+		assertTrue(winAndLoseChecker.hasPlayerLost(this.playerTwo));
 	}
-	
-	@Test
-	public void testHasPlayer1WonWithCrystalAbove400() {
-		this.player1.getMagicLab().addStock(444);
-		WinAndLoseChecker winAndLoseChecker = new WinAndLoseResourceRageChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player1));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player2));
-	}
-	
-	@Test
-	public void testHasPlayer2WonWithCrystalAbove400() {
-		this.player2.getMagicLab().addStock(444);
-		WinAndLoseChecker winAndLoseChecker = new WinAndLoseResourceRageChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player2));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player1));
-	}
-	
-	@Test
-	public void testHasPlayer1WonWithBrickAbove400() {
-		this.player1.getMine().addStock(444);
-		WinAndLoseChecker winAndLoseChecker = new WinAndLoseResourceRageChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player1));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player2));
-	}
-	
-	@Test
-	public void testHasPlayer2WonWithBrickAbove400() {
-		this.player2.getMine().addStock(444);
-		WinAndLoseChecker winAndLoseChecker = new WinAndLoseResourceRageChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player2));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player1));
-	}
-	
-	@Test
-	public void testHasPlayer1WonWithMonstersAbove400() {
-		this.player1.getDungeon().addStock(444);
-		WinAndLoseChecker winAndLoseChecker = new WinAndLoseResourceRageChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player1));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player2));
-	}
-	
-	@Test
-	public void testHasPlayer2WonWithMonstersAbove400() {
-		this.player2.getDungeon().addStock(444);
-		WinAndLoseChecker winAndLoseChecker = new WinAndLoseResourceRageChecker();
-		assertTrue(winAndLoseChecker.hasPlayerWon(this.player2));
-		assertTrue(winAndLoseChecker.hasPlayerLost(this.player1));
-	}
-	
-	
 }
