6f32f8cf6eca48d6dce021496d028588b410be0c
Daniel <Daniel@172.30.1.235>
2012-11-17 15:14:50 +0100

    javadoc for winAndLoseChecker

--- src/main/java/herbstJennrichLehmannRitter/engine/controller/WinAndLoseChecker.java
+++ src/main/java/herbstJennrichLehmannRitter/engine/controller/WinAndLoseChecker.java
@@ -2,6 +2,11 @@ package herbstJennrichLehmannRitter.engine.controller;
 
 import herbstJennrichLehmannRitter.engine.model.Player;
 
+/** Description of WinAndLoseChecker Class
+ *  This abstract Class implements two boolean, which determine if the player has won or lost.
+ *  This is the interface from the server that answers to questions from the client.
+ */
+
 public abstract class WinAndLoseChecker {
 
 	public abstract boolean hasPlayerWon(Player player);
