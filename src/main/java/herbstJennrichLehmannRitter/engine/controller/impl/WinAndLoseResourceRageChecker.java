e3fa6ce576b5222a5de298856d2c6d15aa1b0773
Daniel <Daniel@172.30.1.235>
2012-11-17 15:57:05 +0100

    javadoc for ressource rage and tower building checker

--- src/main/java/herbstJennrichLehmannRitter/engine/controller/impl/WinAndLoseResourceRageChecker.java
+++ src/main/java/herbstJennrichLehmannRitter/engine/controller/impl/WinAndLoseResourceRageChecker.java
@@ -3,6 +3,10 @@ package herbstJennrichLehmannRitter.engine.controller.impl;
 import herbstJennrichLehmannRitter.engine.controller.WinAndLoseChecker;
 import herbstJennrichLehmannRitter.engine.model.Player;
 
+/** Description of WinAndLoseResourceRageChecker Class
+ *  This Class extends the WindAndLoseChecker for the game mode Ressource Rage.
+ */
+
 public class WinAndLoseResourceRageChecker extends WinAndLoseChecker {
 
 	@Override
