fbda102568316f4aa4481ca183e9fb72896565e3
Daniel <Daniel@172.30.1.235>
2012-11-17 14:24:32 +0100

    javadoc for gameClient

--- src/main/java/herbstJennrichLehmannRitter/client/GameClient.java
+++ src/main/java/herbstJennrichLehmannRitter/client/GameClient.java
@@ -2,11 +2,11 @@ package herbstJennrichLehmannRitter.client;
 
 import herbstJennrichLehmannRitter.ui.UserInterface;
 
-/**
- * 
- * Der GameClient ist die Antwortschnittstelle des Server auf dem Client
- * 
+/** Description of GameClient Class
+ *  This Class extends the UserInterface.
+ *  This is the interface from the server that answers to questions from the client.
  */
+
 public interface GameClient extends UserInterface {
 
 }
