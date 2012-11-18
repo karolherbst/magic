30f9cf63dda55b810083c182f861d064f18bd0d4
Sebastian Jennrich <sebastian.jennrich@gmail.de>
2012-11-18 15:02:45 +0100

    remove unused imports

--- src/main/java/herbstJennrichLehmannRitter/engine/Globals.java
+++ src/main/java/herbstJennrichLehmannRitter/engine/Globals.java
@@ -10,10 +10,8 @@ import herbstJennrichLehmannRitter.server.NetworkServer;
 import herbstJennrichLehmannRitter.server.impl.GameServerImpl;
 import herbstJennrichLehmannRitter.server.impl.NetworkServerImpl;
 
-import java.net.UnknownHostException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
